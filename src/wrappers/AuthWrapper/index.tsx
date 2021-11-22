da05628a6bdb3d574b351576a6243ed0d7b8057f
viet.ha <viet.ha@amela.vn>
2021-11-22 13:48:43 +0700

    base reactjs file

---
+++ src/wrappers/AuthWrapper/index.tsx
@@ -0,0 +1,34 @@
+import React, { lazy, Suspense } from 'react';
+import Cookies from 'js-cookie';
+import PageHeader from 'components/PageHeader';
+import SideNav from 'components/SideNav';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import styles from './styles.module.scss';
+import { useQuery } from 'react-query';
+import { getProfile } from 'api/profile';
+import useProfile from 'hooks/useProfile';
+
+const Tasks = lazy(() => import('pages/Tasks'));
+
+export default function PageWrapper() {
+  const isAuthenticated = !!Cookies.get('token');
+  const { profile } = useProfile(isAuthenticated);
+
+  if (!isAuthenticated) return <Redirect to="/login" />;
+  if (!profile) return null;
+  return (
+    <div className={styles.pageWrapper}>
+      <SideNav />
+      <div className={styles.mainWrapper}>
+        <PageHeader />
+        <div className={styles.pageContent}>
+          <Suspense fallback={null}>
+            <Switch>
+              <Route path="/tasks" component={Tasks} />
+            </Switch>
+          </Suspense>
+        </div>
+      </div>
+    </div>
+  );
+}
