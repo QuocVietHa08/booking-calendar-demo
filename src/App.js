7dc9bebbd4447d0051b8967f3c49ebdca1f94d8b
Edward Ha <admin@MacBook-Pro-cua-Admin.local>
2023-09-23 21:54:42 +0700

    feature: implement react-big-calendar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useMemo, useState } from "react";
+import events from "./events";
+import { Calendar, Views, momentLocalizer } from "react-big-calendar";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import moment from 'moment'
+
+const DragAndDropCalendar = withDragAndDrop(Calendar);
 
 function App() {
+  const [myEvents, setMyEvents] = useState(events);
+  const localizer = momentLocalizer(moment)
+
+  const moveEvent = useCallback(
+    ({ event, start, end, isAllDay: droppedOnAllDaySlot = false }) => {
+      const { allDay } = event;
+      if (!allDay && droppedOnAllDaySlot) {
+        event.allDay = true;
+      }
+
+      setMyEvents((prev) => {
+        const existing = prev.find((ev) => ev.id === event.id) ?? {};
+        const filtered = prev.filter((ev) => ev.id !== event.id);
+        return [...filtered, { ...existing, start, end, allDay }];
+      });
+    },
+    [setMyEvents]
+  );
+
+  const resizeEvent = useCallback(
+    ({ event, start, end }) => {
+      setMyEvents((prev) => {
+        const existing = prev.find((ev) => ev.id === event.id) ?? {};
+        const filtered = prev.filter((ev) => ev.id !== event.id);
+        return [...filtered, { ...existing, start, end }];
+      });
+    },
+    [setMyEvents]
+  );
+
+  const defaultDate = useMemo(() => new Date(2023, 9, 23), [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="height600">
+        <div>hello</div>
+        <DragAndDropCalendar
+          defaultDate={defaultDate}
+          defaultView={Views.DAY}
+          events={myEvents}
+          localizer={localizer}
+          onEventDrop={moveEvent}
+          onEventResize={resizeEvent}
+          popup
+          resizable
+          resourceTitleAccessor={(resource) => <div>{resource?.name}</div>}
+          resources= {  [{id: 1, name: 'John'}, {id:2, name: 'Linda'}]  }
+        />
+
+        
+      </div>
     </div>
   );
 }
